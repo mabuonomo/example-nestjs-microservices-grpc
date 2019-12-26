ad2e441e261d8adda136c9b17301e1d81565c1c3
Mario Buonomo <ma.buonomo@gmail.com>
2019-12-26 20:17:59 +0100

    init

--- micr1/src/main.ts
+++ micr1/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: 'http://localhost:6379',
+      protoPath: join(__dirname, '../micr1.proto'),
+      package: 'micro1',
+    },
+  });
+
+  // tslint:disable-next-line: no-console
+  app.listen(() => console.log('Microservice is listening'));
 }
 bootstrap();
